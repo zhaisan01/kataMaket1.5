f57d1e7edcafc04b49f07320e800a1b213a7dd1f
zhaisan <zhaisan.moon@gamil.com>
2022-01-27 12:09:21 +0600

    added slider, pogination, style

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,38 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1.3,
-  spaceBetween: 0,
-  centeredSlides: true,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+let mediaQuerySize = window.screen.width;
+const minimizerList = document.querySelector(".brands__container");
+const btnShowMoreBrands = document.querySelector(".brands__button");
+
+function sliderEnable() {
+  if (mediaQuerySize <= 768) {
+    let swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1.2,
+      spaceBetween: 0,
+      centeredSlides: true,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+    btnShowMoreBrands.style.display = "none";
+  } else {
+    let deleteClassNemeSwiperSlide = document.querySelectorAll(".swiper-slide");
+    deleteClassNemeSwiperSlide.forEach((e) => {
+      e.classList.toggle("swiper-slide");
+    });
+  }
+}
+
+sliderEnable();
+
+btnShowMoreBrands.addEventListener("click", showMoreBrands);
+function showMoreBrands() {
+  minimizerList.classList.toggle("brands__minimized");
+  btnShowMoreBrands.classList.toggle("brands__button--active");
+
+  if (minimizerList.classList.contains("brands__minimized")) {
+    btnShowMoreBrands.textContent = "Скрыть";
+  } else {
+    btnShowMoreBrands.textContent = "Показать все";
+  }
+}
